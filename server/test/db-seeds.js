4195d13481f3b7620a2a77bc70f2225d1104615b
Robert Mccreary <fooyinfighter@gmail.com>
2022-08-20 10:56:37 +0800

    add db-seeds for dialog lines

--- server/test/db-seeds.js
+++ server/test/db-seeds.js
@@ -1,7 +1,37 @@
 module.exports = {
   dialogs: [
-    { title: "First Dialog" },
-    { title: "Second Dialog" },
-    { title: "Thrid Dialog" },
+    { title: "First Dialog", characters: ["Romeo", "Juliet"] },
+    { title: "Second Dialog", characters: ["Rose", "Jack"] },
+    { title: "Thrid Dialog", characters: ["Hamlet"] },
+  ],
+  lines: [
+    [],
+    [
+      {
+        content: "Promise me now.",
+        character: "Jack",
+        audio: {
+          url: "https://cloudinary.com/1234",
+          audio_id: "1234",
+        },
+      },
+      {
+        content: "I promise.",
+        character: "Rose",
+        audio: {
+          url: "https://cloudinary.com/12345",
+          audio_id: "1234",
+        },
+      },
+      {
+        content: "Never let go.",
+        character: "Jack",
+      },
+      {
+        content: "I'll never let go.",
+        character: "Rose",
+      },
+    ],
+    [],
   ],
 };
