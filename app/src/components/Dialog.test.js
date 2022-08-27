8d1914a42df2a9086e78fc03c66074dcd5220dce
Robert Mccreary <fooyinfighter@gmail.com>
2022-08-27 12:54:38 +0800

    added tests for Dialog user input

--- app/src/components/Dialog.test.js
+++ app/src/components/Dialog.test.js
@@ -1,7 +1,15 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent, act } from "@testing-library/react";
 
 jest.mock("../models/api");
 jest.mock("../withAuth");
+
+import {
+  getCalled,
+  postCalled,
+  putCalled,
+  deleteCalled,
+  resetMockCalls,
+} from "../models/api";
 
 import DialogView from "./DialogView";
 import DialogUpdate from "./DialogUpdate";
@@ -12,8 +20,13 @@ import testData from "../__test__/db-seeds";
 const testDialogs = testData.dialogs;
 
 describe("Dialog CRUD", () => {
+  beforeEach(() => {
+    resetMockCalls();
+  });
+
   test("renders DialogView", async () => {
-    const testRoute = `/dialogs/${testDialogs[0]._id}`;
+    const dialog_id = testDialogs[0]._id;
+    const testRoute = `/dialogs/${dialog_id}`;
     render(
       <MemoryRouter initialEntries={[testRoute]}>
         <Routes>
@@ -23,13 +36,19 @@ describe("Dialog CRUD", () => {
     );
     const myDialogsElement = await screen.findByText(/First Dialog/);
     expect(myDialogsElement).toBeInTheDocument();
+    expect(getCalled).toHaveBeenCalledWith(testRoute);
   });
   test("renders DialogUpdate", async () => {
-    const testRoute = `/dialogs/${testDialogs[1]._id}/edit`;
+    const dialog_id = testDialogs[1]._id;
+    const testRoute = `/dialogs/${dialog_id}/edit`;
     render(
       <MemoryRouter initialEntries={[testRoute]}>
         <Routes>
           <Route path="/dialogs/:dialog_id/edit" element={<DialogUpdate />} />
+          <Route
+            path="/dialogs/:dialog_id"
+            element={<div>Mock Updated Dialog</div>}
+          />
         </Routes>
       </MemoryRouter>
     );
@@ -40,13 +59,47 @@ describe("Dialog CRUD", () => {
     const role2 = await screen.findByPlaceholderText("Role 2");
     expect(role1).toHaveValue("Rose");
     expect(role2).toHaveValue("Jack");
+    fireEvent.change(dialogTitleInput, { target: { value: "The Titanic" } });
+    await fireEvent.click(await screen.findByText("Submit"));
+    expect(putCalled).toHaveBeenCalledWith(`/dialogs/${dialog_id}`, {
+      title: "The Titanic",
+      characters: ["Rose", "Jack"],
+    });
+    // re-route to
+    const myDialogsElement = await screen.findByText(/Mock Updated Dialog/);
+    expect(myDialogsElement).toBeInTheDocument();
+  });
+  test("deletes Dialog from DialogUpdate", async () => {
+    const testRoute = `/dialogs/${testDialogs[1]._id}/edit`;
+    render(
+      <MemoryRouter initialEntries={[testRoute]}>
+        <Routes>
+          <Route path="/dialogs/:dialog_id/edit" element={<DialogUpdate />} />
+          <Route path="/dialogs" element={<div> Mock Dialog List </div>} />
+        </Routes>
+      </MemoryRouter>
+    );
+    const dialogTitleInput = await screen.findByPlaceholderText("Dialog Title");
+    expect(dialogTitleInput).toBeInTheDocument();
+    expect(dialogTitleInput).toHaveValue("Second Dialog");
+    const deleteButton = await screen.findByText("Delete");
+    await fireEvent.click(deleteButton);
+    expect(deleteCalled).toHaveBeenCalled();
+    // check redirect
+    const listComponent = await screen.findByText("Mock Dialog List");
+    expect(listComponent).toBeInTheDocument();
   });
   test("renders DialogCreate", async () => {
     const testRoute = "/dialogs/new";
+    let testHistory, testLocation;
     render(
       <MemoryRouter initialEntries={[testRoute]}>
         <Routes>
           <Route path="/dialogs/new" element={<DialogCreate />} />
+          <Route
+            path="/dialogs/123created123"
+            element={<div>Created New Dialog</div>}
+          />
         </Routes>
       </MemoryRouter>
     );
@@ -56,5 +109,19 @@ describe("Dialog CRUD", () => {
     const role2 = await screen.findByPlaceholderText("Role 2");
     expect(role1).toBeInTheDocument();
     expect(role2).toBeInTheDocument();
+
+    fireEvent.change(dialogTitleInput, { target: { value: "The Titanic" } });
+    fireEvent.change(role1, { target: { value: "Jack" } });
+    fireEvent.change(role2, { target: { value: "Rose" } });
+    expect(role1.value).toBe("Jack");
+    expect(role2.value).toBe("Rose");
+    await fireEvent.click(await screen.findByText("Submit"));
+    expect(postCalled).toHaveBeenCalledWith("/dialogs", {
+      title: "The Titanic",
+      characters: ["Jack", "Rose"],
+    });
+    // re-route to
+    const myDialogsElement = await screen.findByText(/Created New Dialog/);
+    expect(myDialogsElement).toBeInTheDocument();
   });
 });
--- app/src/models/__mocks__/api.js
+++ app/src/models/__mocks__/api.js
@@ -8,10 +8,18 @@ const wrapAxiosCall = (data) => {
   return Promise.resolve({ data: data });
 };
 
+let getCalled, postCalled, putCalled, deleteCalled;
+
+getCalled = jest.fn();
+postCalled = jest.fn();
+putCalled = jest.fn();
+deleteCalled = jest.fn();
+
 const setupEchoDialogAuth = jest.fn();
 const getApiFetcher = () => {
   return {
     get: (url) => {
+      getCalled(url);
       if (url === "/dialogs") {
         return wrapAxiosCall(
           sampleDb.dialogs.filter((d) => d.user_id === USER_ID)
@@ -22,18 +30,42 @@ const getApiFetcher = () => {
       }
     },
     post: (url, data) => {
-      if (url === "/dialogs/new") {
+      postCalled(url, data);
+      if (url === "/dialogs") {
         return wrapAxiosCall({ ...data, _id: "123created123" });
       }
     },
-    update: (url, data) => {
+    put: (url, data) => {
+      putCalled(url, data);
       if (url.match(/^\/dialogs\/\w+/)) {
         const dialog_id = url.match(/^\/dialogs\/(\w+)/)[1];
         const dialog = sampleDb.dialogs.find((d) => d._id === dialog_id);
         return wrapAxiosCall({ ...dialog, ...data });
       }
     },
+    delete: (url) => {
+      deleteCalled(url);
+      if (url.match(/^\/dialogs\/\w+/)) {
+        const dialog_id = url.match(/^\/dialogs\/(\w+)/)[1];
+        return wrapAxiosCall({ message: "Deleted Document " + dialog_id });
+      }
+    },
   };
 };
 
-export { setupEchoDialogAuth, getApiFetcher };
+const resetMockCalls = () => {
+  getCalled = jest.fn();
+  postCalled = jest.fn();
+  putCalled = jest.fn();
+  deleteCalled = jest.fn();
+};
+
+export {
+  setupEchoDialogAuth,
+  getApiFetcher,
+  getCalled,
+  postCalled,
+  putCalled,
+  deleteCalled,
+  resetMockCalls,
+};
