f69c774d4d7b62fe2e408fa4c7bb0ec6633aa30a
Robert Mccreary <fooyinfighter@gmail.com>
2022-08-29 13:05:07 +0800

    lengthen api fetch timeout limit

--- app/src/models/api.js
+++ app/src/models/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const baseURL = `${process.env.REACT_APP_API_PREFIX}`;
 const apiInstance = axios.create({
   baseURL: baseURL, // "http://localhost:5000/api/v1",
-  timeout: 1000,
+  timeout: 2000,
 });
 
 const setupEchoDialogAuth = ({ token }) => {
