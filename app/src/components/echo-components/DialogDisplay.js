ca3f56963a4384663597a1c47a1e4763815ff09a
Robert Mccreary <fooyinfighter@gmail.com>
2022-08-31 18:47:22 +0800

    update home page and larger area to click on Dialog Listing.

--- app/src/components/echo-components/DialogDisplay.js
+++ app/src/components/echo-components/DialogDisplay.js
@@ -2,22 +2,31 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
+import withParams from "../../withParams";
 
 import "./Echo.scss";
 
 class DialogDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+    this.goToEcho = this.goToEcho.bind(this);
+  }
+  goToEcho() {
+    console.log("go");
+    this.props.navigate(`/echo/${this.props.dialog._id}`);
+  }
   render() {
     const { dialog } = this.props;
-    console.log(this.props);
-    console.log(dialog);
     return (
       <Card className="dialog-display">
         <Card.Header>
           <h1> {dialog.title} </h1>
         </Card.Header>
         <Card.Body>
-          <p>Character 1: {dialog.characters[0]}</p>
-          <p>Character 2: {dialog.characters[1] || "_"}</p>
+          <p onClick={this.goToEcho}>Character 1: {dialog.characters[0]}</p>
+          <p onClick={this.goToEcho}>
+            Character 2: {dialog.characters[1] || "_"}
+          </p>
           <Button as={Link} to={`/echo/${dialog._id}`}>
             Make an Echo
           </Button>
@@ -27,4 +36,4 @@ class DialogDisplay extends React.Component {
   }
 }
 
-export default DialogDisplay;
+export default withParams(DialogDisplay);
