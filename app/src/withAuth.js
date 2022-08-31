2d020e32c87c4d5ee6331eb2d06107e9c1b15851
Robert Mccreary <fooyinfighter@gmail.com>
2022-08-31 21:26:10 +0800

    redirect to auth if withAuth.setupAccessToken returns Login required

--- app/src/withAuth.js
+++ app/src/withAuth.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { useAuth0 } from "@auth0/auth0-react";
+import { useAuth0, loginWithRedirect } from "@auth0/auth0-react";
 
 import { setupEchoDialogAuth } from "./models/api";
 
@@ -31,6 +31,9 @@ const withAuth = (Component) => {
         })
         .catch((e) => {
           console.log(e);
+          if (e.message === "Login required") {
+            myAuth0.loginWithRedirect();
+          }
           return e;
         });
     };
